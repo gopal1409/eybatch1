51312869159f3975a70b8a79292ba4b4e4e4bd7f
gopal1409 <gopal1409@gmail.com>
2023-02-12 15:17:33 +0530

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,15 @@
+import json
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    body = "Hello From Lambda"
+    statusCode=200
+    return {
+        "statusCode": statusCode,
+        "body": json.dumps(body),
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    }
